e1393c7b06ae9dfe3713bd291f99be2e986d85a1
João Marcos <marcos.silva@ebix.com>
2019-11-28 13:51:05 -0300

    Criação dos metodos: handleHttpMessageNotReadable - para tratamento de post com valor inválido, handleMethodArgumentNotValid - para valores abaixo e acima do permitido no campo,  criarListaDeErros - responsável por criar lista de erro e a classe Erro contendo os atributos mnsagemUsuario e mensagemDesenvolvedor.

--- src/main/java/com/application/moneyapi/api/execeptionhandler/MoneyExeceptionHandler.java
+++ src/main/java/com/application/moneyapi/api/execeptionhandler/MoneyExeceptionHandler.java
@@ -7,9 +7,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @ControllerAdvice //Classe responsável por observar aplicação
 public class MoneyExeceptionHandler extends ResponseEntityExceptionHandler {
@@ -18,12 +25,30 @@ public class MoneyExeceptionHandler extends ResponseEntityExceptionHandler {
     private MessageSource messageSource;
 
     @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
         String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
         String mensagemDensenvolvedor = ex.getCause().toString();
-        return handleExceptionInternal(ex, new Erro(mensagemDensenvolvedor, mensagemUsuario), headers, HttpStatus.BAD_REQUEST, request);
+        List<Erro> erros = Arrays.asList(new Erro(mensagemDensenvolvedor, mensagemUsuario));
+        return handleExceptionInternal(ex, erros , headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request){
+        List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+        return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    private List<Erro> criarListaDeErros(BindingResult bindingResult){
+        List<Erro> erros = new ArrayList<>();
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+            String mensagemDesenvolvedor = fieldError.toString();
+            erros.add(new Erro( mensagemDesenvolvedor, mensagemUsuario));
+        }
+
+        return erros;
     }
 
     public static class Erro {
@@ -31,7 +56,7 @@ public class MoneyExeceptionHandler extends ResponseEntityExceptionHandler {
         private String memensagemUsuario;
         private String mensagemDensenvolvedor;
 
-        public Erro(String mensagemDensenvolvedor, String mensagemUsuario) {
+        private Erro(String mensagemDensenvolvedor, String mensagemUsuario) {
             this.memensagemUsuario = mensagemUsuario;
             this.mensagemDensenvolvedor = mensagemDensenvolvedor;
         }
@@ -39,15 +64,12 @@ public class MoneyExeceptionHandler extends ResponseEntityExceptionHandler {
         public String getMemensagemUsuario() {
             return memensagemUsuario;
         }
-
         public void setMemensagemUsuario(String memensagemUsuario) {
             this.memensagemUsuario = memensagemUsuario;
         }
-
         public String getMensagemDensenvolvedor() {
             return mensagemDensenvolvedor;
         }
-
         public void setMensagemDensenvolvedor(String mensagemDensenvolvedor) {
             this.mensagemDensenvolvedor = mensagemDensenvolvedor;
         }
