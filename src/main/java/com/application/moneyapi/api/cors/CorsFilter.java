7edfc3b045d5e63c6fc6956844428b4b2370eda9
joaomarcosjmos <joaomarcosjmos.ans@gmail.com>
2020-04-07 20:05:53 -0300

    Implementação da classe CorsFilter para utilização da requisições

---
+++ src/main/java/com/application/moneyapi/api/cors/CorsFilter.java
@@ -0,0 +1,46 @@
+package com.application.moneyapi.api.cors;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+       private String origimPermitida = "http://localhost:8000"; //TODO: configurar para diferentes ambientes
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+
+        response.setHeader("Access-Control-Allow-Origin", origimPermitida);
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if("OPTIONS".equals(request.getMethod()) && origimPermitida.equals(request.getHeader("Origin"))){
+
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+            response.setHeader("Access-Control-Max-Age", "3600");
+
+            response.setStatus(HttpServletResponse.SC_OK);
+        }else {
+            chain.doFilter(req, resp);
+        }
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
