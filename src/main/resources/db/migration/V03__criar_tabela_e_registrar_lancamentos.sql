914ed8a8f1d544a0d3e581e74c20368d21597ce6
joaomarcosjmos <joaomarcosjmos.ans@gmail.com>
2020-08-03 23:14:34 -0300

    Atualização do arquivo de migração.

--- src/main/resources/db/migration/V03__criar_tabela_e_registrar_lancamentos.sql
+++ src/main/resources/db/migration/V03__criar_tabela_e_registrar_lancamentos.sql
@@ -8,8 +8,8 @@ CREATE TABLE lancamento(
     tipo VARCHAR(20) NOT NULL,
     codigo_categoria BIGINT(20) NOT NULL,
     codigo_pessoa BIGINT(20) NOT NULL,
-    FOREIGN KEY (codigo_categoria) REFERENCES moneyapi.categoria(codigo),
-    FOREIGN KEY (codigo_pessoa) REFERENCES moneyapi.pessoa(codigo)
+    FOREIGN KEY (codigo_categoria) REFERENCES moneyapiconsult.categoria(codigo),
+    FOREIGN KEY (codigo_pessoa) REFERENCES moneyapiconsult.pessoa(codigo)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) VALUES ('Salario mensal', '2017-06-10', null, 6500.00, 'Distribuição de lucros', 'RECEITA', 5, 1);
